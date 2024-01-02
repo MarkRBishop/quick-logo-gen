7792761ef51b2dfbdb95037e98796432d9939b77
MarkRBishop <mrb.8@hotmail.com>
2024-01-02 12:08:39 -0600

    Commented out the test file

--- tests/shapes.test.js
+++ tests/shapes.test.js
@@ -1,6 +1,7 @@
+//Imports the Shape and Triangle classes from shapes.js
 const { Shape, Triangle } = require('../lib/shapes');
 
-
+//Test for the Shape class to set the color of the shape correctly
 describe('Shape class', () => {
   test('should set shape color correctly', () => {
     const shape = new Shape();
@@ -9,6 +10,7 @@ describe('Shape class', () => {
     expect(shape.shapeColor).toBe(shapeColor);
   });
 
+  //Testing to make sure the text is correctly set
   test('should set text correctly', () => {
     const shape = new Shape();
     const text = 'Hello';
@@ -16,12 +18,15 @@ describe('Shape class', () => {
     expect(shape.text).toBe(text);
   });
 
+  //test to make sure the text color is correct
   test('should set text color correctly', () => {
     const shape = new Shape();
     const textColor = 'blue';
     shape.setTextColor(textColor);
     expect(shape.textColor).toBe(textColor);
   });
+
+  //test to make sure the the shape and color is correct.
   test('should set shape color correctly', () => {
     const shape = new Triangle();
     shape.setShapeColor("blue");
